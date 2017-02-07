122e71f853478db1978eb96d5dc6b4054c451bde
ian yeh <iansrc0811@hotmail.com>
2017-02-07 20:46:52 +0800

    temporory modify show problems

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,8 +8,11 @@ class SearchController < ApplicationController
     @book_items = Crawler.start_crawler(input) 
     
     if @book_items
-      #render partial:'/lists/search_books' 
-      render template: '/lists/show'
+      #
+      @list = List.find(params[:list_id])
+      #render template: '/lists/show'
+      render partial:'/lists/search_books' 
+
     else
       render status: :not_found, nothing: true 
     end
