4400a99b252b3d6951c7ee243379f8043b1035f1
ian yeh <iansrc0811@hotmail.com>
2017-02-23 17:05:35 +0800

    modify the bug that unlog user can delete items from lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,28 +9,18 @@ puts "1 user created"
 @user3 = User.create(email: "ian3@example.com", password: 'testtest', 
         password_confirmation: "testtest" , first_name: "ian3", last_name: "test", nick_name: "Ian3Test")
 puts "1 user created"
-
-AdminUser.create(email: "ian_admin@example.com", password: 'testtest', 
-        password_confirmation: "testtest" , first_name: "ian", last_name: "admin", nick_name: "IanAdmin")
-puts "1 admin has been created"
-
-15.times do |list|
-  @list = List.create!(name: "書單 #{list}", description:" Description #{list}", 
-    user_id: @user.id)
-
-  20.times do |article|
-    @article = Article.create!(title: "書單 #{list} No. #{article} Article of List #{list}")
-    @article.lists << @list  # many to many association
-  end
-end
-
-2.times do |list|
-  @list = List.create!(name: "List #{list}", description:" Description #{list}", 
-    user_id: @user2.id)
-
-  3.times do |article|
-    @article = Article.create!(title: "List #{list} No. #{article} Article of List #{list}")
-    @article.lists << @list  # many to many association
-  end
-end
-puts "Lists and Articles created"
+=begin
+  t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "nick_name"
+=end
