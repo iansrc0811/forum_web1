9642ae0284dc4baa3a7a768c4bbcc60253cfc41d
ian yeh <iansrc0811@hotmail.com>
2017-03-17 20:08:56 +0800

    some moderfy

--- app/assets/javascripts/result.js
+++ app/assets/javascripts/result.js
@@ -2,19 +2,24 @@ var init_item_lookup;
 
 init_item_lookup = function(){
   $('#item-lookup-form').on('ajax:before', function(event, data, status){
+    
     show_spinner();
+
   });
   $('#item-lookup-form').on('ajax:after', function(event, data, status){
     hide_spinner();
   });
   $('#item-lookup-form').on('ajax:success', function(event, data, status){
     $('#item-lookup').replaceWith(data);
+
     init_item_lookup();
   });
   $('#item-lookup-form').on('ajax:error', function(event, xhr, status, error){
     hide_spinner();
     $('#item-lookup-results').replaceWith('');
-    $('#item-lookup-errors').replaceWith('ajax error: ' + error)
+    $('#item-lookup-errors').replaceWith(
+      '<h3>  找不到商品, 請檢查輸入值</h3>')
+
   });
 }
 
